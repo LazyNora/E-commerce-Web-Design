5cacc2d5dac4b630abb0070f4524b3047a2e1073
LazyNora <lazynora@outlook.com>
2024-03-16 01:19:52 +0700

    first commit

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+	flex: 1;
+	margin: 3px;
+	height: 70vh;
+	position: relative;
+`;
+const Image = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+const Info = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+const Title = styled.h1`
+	color: white;
+	margin-bottom: 20px;
+`;
+const Button = styled.button`
+	font-size: 20px;
+	padding: 10px;
+	background-color: white;
+	color: gray;
+	cursor: pointer;
+	border: 1px solid transparent;
+	border-radius: 10px;
+	margin: 20px;
+	transition: all 0.5s ease;
+	&:hover {
+		background-color: #f8e9d6;
+		color: black;
+		border: 1px solid black;
+	}
+`;
+
+const CategoryItem = ({ item }) => {
+	return (
+		<Container>
+			<Image src={item.img} />
+			<Info>
+				<Title>{item.title}</Title>
+				<Button>SHOP NOW</Button>
+			</Info>
+		</Container>
+	);
+};
+
+export default CategoryItem;
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Newsletter = () => {
+	return <Container>Newsletter</Container>;
+};
+
+export default Newsletter;
