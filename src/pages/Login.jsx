153caf2f6dc66b35a1ff06fde399e6e7fd5943c3
tanoni3k <tancao6359@gmail.com>
2024-05-03 19:24:11 +0700

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,20 +6,34 @@ import Categories from "../components/Categories";
 import Products from "../components/Products";
 import Footer from "../components/Footer";
 import Carousel from "../components/Carousel";
-import {
-	RemoveRedEye
-} from "@material-ui/icons";
+import { RemoveRedEye } from "@material-ui/icons";
 
 const Login = () => {
   return (
     <div>
-      <Announcement />
-      <Navbar />
-     <div className="Login">
-	 Login
-	  <svg className="Eye" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"></path></svg>
-	 </div>
-      <Footer />
+      {/* <Announcement />
+      <Navbar /> */}
+      <div className="login">
+       <h2>Log In</h2>
+
+       
+          <div className="input-box item">
+            <input type="text" placeholder="Your Email" />
+          </div>
+          <div className="input-box item">
+            <input type="text" placeholder="Your Password" />
+          </div>
+       
+
+        <a href="#">Forgot your password?</a>
+
+        <div className="signin-button item">
+          <button>SIGN IN</button>
+        </div>
+
+        <a href="#">Register</a>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 };
