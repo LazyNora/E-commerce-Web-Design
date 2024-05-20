85e3a0318a53f951ce5b68777cbd372ca6eb3e09
LazyNora <lazynora@outlook.com>
2024-05-20 23:11:44 +0700

    ProductDetails

--- js/ProductDetails.js
+++ js/ProductDetails.js
@@ -18,3 +18,29 @@ metafields &&
     `;
 		tabsWrap.insertBefore(tabBlock, reviewTab);
 	});
+
+const tabHeaders = document.querySelectorAll(".tab-header");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabHeaders.forEach((tabHeader, index) => {
+	tabHeader.addEventListener("click", () => {
+		tabHeaders.forEach((tabHeader) => tabHeader.classList.remove("active"));
+		tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+		tabHeader.classList.add("active");
+		tabContents[index].classList.add("active");
+	});
+});
+
+const accordionItems = document.querySelectorAll(".accordion-item");
+const accordionButtons = document.querySelectorAll(".accordion-button");
+const accordionContents = document.querySelectorAll(".accordion-content");
+
+accordionButtons.forEach((accordionButton, index) => {
+	accordionButton.addEventListener("click", () => {
+		accordionItems[index].classList.toggle("open");
+		accordionContents[index].classList.toggle("open");
+		accordionContents[index].classList[
+			accordionContents[index].classList.contains("open") ? "add" : "remove"
+		]("overflow-hidden");
+	});
+});
